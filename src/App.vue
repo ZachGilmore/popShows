a05c4825461b13f50856615011efe769194f3c82
ZachGilmore <zachg_93@hotmail.com>
2021-05-12 15:45:40 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <Navigation />
+    <div class="container">
+      <div class="row">
+        <Card v-for="movie in movies.slice(0, 4)" v-bind:key=movie.id :movieobj=movie class="col-12 col-md-3"></Card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navigation from "./components/Navigation.vue";
+import Card from "./components/Card.vue";
+import axios from "axios";
+
+export default {
+  name: "App",
+  components: {
+    Navigation,
+    Card,
+  },
+  data()
+  {
+    return{
+      // movies:[
+      //   {id:1, title:'Top Gun', overview:"A movie about fighter pilots", poster_path: "topgun.jpg"},
+      //   {id:2, title:'The Matrix', overview:"A movie about fighter pilots", poster_path: "matrix.jpg"}
+        
+      // ]
+      movies: ""
+    }
+  },
+  mounted()
+  {
+    axios.get("https://api.themoviedb.org/3/tv/popular?api_key=cef7ecc04fe7078af61575957da191fb&language=en-US&page=1")
+                .then( (response) =>{
+                    this.movies = response.data.results;
+                })
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card" style="width: 18rem">
+    <img :src="`https://image.tmdb.org/t/p/w500${movieobj.poster_path}`" class="card-img-top">
+    <div class="card-body">
+      <h5 class="card-title">{{movieobj.name}}</h5>
+      <p class="card-text">
+        {{movieobj.overview}}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props:['movieobj']
+};
+</script>
